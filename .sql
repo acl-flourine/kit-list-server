65041fc1681606f715a6fb1d1731bd2039b692cb
Jennifer Elton <justjenny40@hotmail.com>
2017-12-14 12:34:01 -0800

    created sql sheet to enter into database

---
+++ .sql
@@ -0,0 +1,56 @@
+CREATE TABLE items (item_id SERIAL PRIMARY KEY, listType VARCHAR(55), item VARCHAR (10),amount VARCHAR (55);
+
+CREATE TABLE users (user_id SERIAL PRIMARY KEY, household INTEGER, heat BOOLEAN, snow BOOLEAN, infant BOOLEAN, child, BOOLEAN, meds BOOLEAN, pets BOOLEAN, #ofdays INTEGER);
+
+
+
+
+INSERT INTO items (listType, item, amount) VALUES ('base', 'flashlight', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'food', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'water', 'per-day');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'batteries', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'radio', 'per-houshold');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'first aid kit', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'personal hygene', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'multi tool', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'medication', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'cell phone charger', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'personal documents', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'cash', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'emergency blankets', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'insect repellant', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'whistle', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'matches lighter', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'propane stove', 'per-houshold');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'extra propane', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('heat', 'light loose clothing', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('heat', 'light meals', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('heat', 'extra water', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('heat', 'hat', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('cold', 'shovel', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('cold', 'gloves', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('cold', 'thermal underwear', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('cold', 'extra layers', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'pet food', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'pet water', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'poop bags', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'extra collar', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'extra leash', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'extra crate or carrier for pet', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('pets', 'pet supplies', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('infant', 'formula', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('infant', 'diapers', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('infant', 'baby wipes', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'therometer', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'vaccination records for family', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'pedialite', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('infant', 'baby blankets', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('infant', 'diaper rash cream', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('infant', 'baby sling', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'can opener', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'books', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('child', 'small games', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('child', 'child hygene items', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('child', 'cards', 'per-person');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'paper and pencils', 'per-household');
+INSERT INTO items (listType, item, amount) VALUES ('base', 'local maps', 'per-household');
